2144053e8236b3cf3cd00a4556ab86d61c150d79
OValya <valya.eto.ya@gmail.com>
2022-08-18 11:23:43 +0300

    feat: add env data

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 import express from 'express';
 import mongoose from "mongoose";
-import fileUpload from "express-fileupload"
-
-const DB_URL = 'mongodb://localhost:27017/test'
-
+//import {PORT, NODE_ENV, MONGO_CONNECTION_STRING} from './common/config.js'
+//import fileUpload from "express-fileupload"
 import routeRouter from "./routing.js";
+import dotenv from 'dotenv';
 
 
-const PORT = process.env.PORT || 5000;
+dotenv.config({
+        // path: path.join(__dirname, './.env')
+});
+
+//const DB_URL = 'mongodb://localhost:27017/test'
+//const DB_URL = 'mongodb+srv://user:user@cluster0.1wfjau8.mongodb.net/testBd?retryWrites=true&w=majority'
+const MONGO_CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING;
+const PORT = process.env.PORT;
+//console.log('port', PORT)
+
+//const PORT = process.env.PORT || 5000;
+//const PORT = 5000;
 const app = express();
 
 app.use(express.json());
@@ -16,8 +26,10 @@ app.use(express.static('static'))
 //app.use(fileUpload({}));
 
 async function startApp(){
-        await mongoose.connect(DB_URL).catch((e) => console.log(e));
-        app.listen(port, () => console.log('server started on localhost: '+ PORT))
+        // console.log(MONGO_CONNECTION_STRING)
+        await mongoose.connect(MONGO_CONNECTION_STRING).catch((e) => console.log(e));
+
+        app.listen(PORT, () => console.log('server started on localhost: '+ PORT))
 }
 
 startApp();
